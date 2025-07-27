bd2be76ecf044664b34ebbe0a84cedc03f21dd00
ruanvalente <contato.ruanvalente@gmail.com>
2025-07-27 18:41:19 -0300

    feat: add shadcn ui config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <div>
-      Notes Share
-    </div>
-  );
+	return (
+		<div>
+			<Button>Click me</Button>
+			Notes Share
+		</div>
+	);
 }
