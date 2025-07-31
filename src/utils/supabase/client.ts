58348c9abbeda9b830adf438ece0b5ebf952e1d5
ruanvalente <ruanopensuselinux@gmail.com>
2025-07-31 12:13:42 -0300

    feat: add supabase client for client components

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,10 @@
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
+
+export function createSubaseClient() {
+	return createClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	);
+}
