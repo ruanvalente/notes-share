be67a5d03b5883433979ac790d1e096dbbcc83fc
ruanvalente <ruanopensuselinux@gmail.com>
2025-08-03 15:18:29 -0300

    refactor: refactor middleware

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,31 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+	const { searchParams, origin } = new URL(request.url);
+	const code = searchParams.get("code");
+	const type = searchParams.get("type");
+	const next = searchParams.get("next") ?? "/";
+
+	if (code) {
+		const supabase = await createClient();
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+		if (!error) {
+			if (type === "recovery") {
+				return NextResponse.redirect(`${origin}/reset-password`);
+			}
+			return NextResponse.redirect(`${origin}${next}`);
+		} else {
+			const errorMessage = encodeURIComponent(error.message);
+			if (type === "recovery") {
+				return NextResponse.redirect(`${origin}/forgot?error=${errorMessage}`);
+			}
+			return NextResponse.redirect(`${origin}/login?error=${errorMessage}`);
+		}
+	}
+
+	return NextResponse.redirect(
+		`${origin}/login?error=${encodeURIComponent("Link inválido")}`
+	);
+}
