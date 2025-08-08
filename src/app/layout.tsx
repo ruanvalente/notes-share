9638230dc22d6105c2f1d8176bff1532124ea279
ruanvalente <ruanopensuselinux@gmail.com>
2025-08-08 11:48:01 -0300

    feat: add toast notification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/sonner";
 import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -82,6 +83,7 @@ export default function RootLayout({
 						</p>
 					</div>
 				</footer>
+				<Toaster richColors position="top-right" />
 			</body>
 		</html>
 	);
